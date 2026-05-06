8cd81b10cc6f64ad0a7805a7926de2a93043fffc
jrjlawas <jayromellawas30@gmail.com>
2026-05-07 00:23:45 +0800

    dynamic VAR Keys

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -1,11 +1,15 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
-
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: "https://tj2rfcdbfrcx3emxk5plwny35u.appsync-api.ap-southeast-2.amazonaws.com/graphql",
+    uri: import.meta.env.VAR_URI,
     headers: {
-      "x-api-key": "da2-c2x5ccvmcnat3ckfn74dzuuofu",
+      "x-api-key": import.meta.env.VAR_KEY,
     },
   }),
   cache: new InMemoryCache(),
 });
+
+// uri: "https://tj2rfcdbfrcx3emxk5plwny35u.appsync-api.ap-southeast-2.amazonaws.com/graphql",
+// headers: {
+//   "x-api-key": "da2-c2x5ccvmcnat3ckfn74dzuuofu",
+// },
